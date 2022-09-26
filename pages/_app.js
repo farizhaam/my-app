9842fdefa59b9d41af58c3c2bfec04d589f487cc
Farizha Aulia Martakusuma <31694612+farizhaam@users.noreply.github.com>
2022-09-27 06:54:52 +0700

    Update _app.js
    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
+import { UserProvider } from '@auth0/nextjs-auth0'
+import Layout from '../comps/Layout'
 import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <UserProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>  
+    </UserProvider>
+  )
 }
 
 export default MyApp
