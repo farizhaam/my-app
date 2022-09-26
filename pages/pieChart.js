979f0d54df205d98e914c2cef163ad7d326b05b7
Farizha Aulia Martakusuma <31694612+farizhaam@users.noreply.github.com>
2022-09-27 06:56:17 +0700

    Create pieChart.js
    initial commit

---
+++ pages/pieChart.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import {
+    Chart,
+    ArcElement,
+    LineElement,
+    BarElement,
+    PointElement,
+    BarController,
+    BubbleController,
+    DoughnutController,
+    LineController,
+    PieController,
+    PolarAreaController,
+    RadarController,
+    ScatterController,
+    CategoryScale,
+    LinearScale,
+    LogarithmicScale,
+    RadialLinearScale,
+    TimeScale,
+    TimeSeriesScale,
+    Decimation,
+    Filler,
+    Legend,
+    Title,
+    Tooltip,
+    SubTitle
+  } from 'chart.js';
+  
+  Chart.register(
+    ArcElement,
+    LineElement,
+    BarElement,
+    PointElement,
+    BarController,
+    BubbleController,
+    DoughnutController,
+    LineController,
+    PieController,
+    PolarAreaController,
+    RadarController,
+    ScatterController,
+    CategoryScale,
+    LinearScale,
+    LogarithmicScale,
+    RadialLinearScale,
+    TimeScale,
+    TimeSeriesScale,
+    Decimation,
+    Filler,
+    Legend,
+    Title,
+    Tooltip,
+    SubTitle
+  );
+  
+  import {Doughnut} from 'react-chartjs-2';
+
+  const data = {
+    labels: [
+      'Red',
+      'Green',
+      'Yellow'
+  ],
+  datasets: [{
+    data: [300, 50, 100],
+    backgroundColor: [
+    '#FF6384',
+    '#36A2EB',
+    '#FFCE56'
+    ],
+    hoverBackgroundColor: [
+    '#FF6384',
+    '#36A2EB',
+    '#FFCE56'
+    ]
+  }]
+  };
+  
+  export default () => (
+  <div>
+    <h2>Doughnut Example</h2>
+    <Doughnut
+       data={data}
+       width={400}
+       height={400}
+    />
+  </div>
+  );
